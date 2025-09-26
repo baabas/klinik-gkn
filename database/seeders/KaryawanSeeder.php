0ef7e7b66ff7785777f0eb27276b89528db6f0de
Me <bastianhadinata20@gmail.com>
2025-09-26 20:57:37 +0700

    seeder

--- database/seeders/KaryawanSeeder.php
+++ database/seeders/KaryawanSeeder.php
@@ -16,6 +16,11 @@ class KaryawanSeeder extends Seeder
         // Hapus data lama untuk menghindari duplikasi
         DB::table('karyawan')->delete();
 
-        // Input Data karyawan baru disini
+        DB::table('karyawan')->insert([
+            ['nip' => '198507202009444444', 'nama_karyawan' => 'Fajar Nugroho', 'jabatan' => 'Kepala Seksi', 'kantor' => 'KPP Madya SMG', 'email' => 'fajar.nugroho@example.com', 'alamat' => 'Jl. Simpang Lima No. 1, Semarang', 'agama' => 'Hindu', 'tanggal_lahir' => '1985-07-20'],
+            ['nip' => '198702142010333333', 'nama_karyawan' => 'Oscar Mahendra', 'jabatan' => 'Kepala Seksi', 'kantor' => 'KPP Madya SMG', 'email' => 'oscar.m@example.com', 'alamat' => 'Jl. Papandayan No. 33, Semarang', 'agama' => 'Buddha', 'tanggal_lahir' => '1987-02-14'],
+            ['nip' => '198811222010555555', 'nama_karyawan' => 'Doni Firmansyah', 'jabatan' => 'Fungsional Pemeriksa', 'kantor' => 'KPP Gayam Sari', 'email' => 'doni.f@example.com', 'alamat' => 'Jl. Gajah Mada No. 55, Semarang', 'agama' => 'Katolik', 'tanggal_lahir' => '1988-11-22'],
+
+        ]);
     }
 }
